ed235083d14690c874ae7d53ae3083ae4dc127e1
altesmi <optikkanen@gmail.com>
2019-03-16 22:57:50 +0200

    static frontpage working

--- website/settings.py
+++ website/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/mysite/static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/mysite/static/',
+)
