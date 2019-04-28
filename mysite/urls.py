800565142b37102534a8f897b32a162248e2a4c0
altesmi <optikkanen@gmail.com>
2019-04-28 18:23:26 +0300

    Added change language link

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('language/<language_code>', views.ActivateLanguageView.as_view(), name='change_language')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import translation
+from django.views.generic.base import View
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'mysite/index.html')
+
+
+class ActivateLanguageView(View):
+    language_code = ''
+    redirect_to = ''
+
+    def get(self, request, *args, **kwargs):
+        self.redirect_to = request.META.get('HTTP_REFERER')
+        self.language_code = kwargs.get('language_code')
+        translation.activate(self.language_code)
+        request.session[translation.LANGUAGE_SESSION_KEY] = self.language_code
+        return redirect(self.redirect_to)
